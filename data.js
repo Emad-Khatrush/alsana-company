d2fab3f735d98c9f49b33011e8da8b8068faa1f4
emadkhatrush <qwe.emad@hotmail.com>
2021-04-01 23:11:20 +0300

    finish english page

--- data.js
+++ data.js
@@ -1,40 +1,55 @@
-
 const dataEn = [{
     id: "1",
-    img: ["/assets/img/project1/1.png", "/assets/img/project1/2.png", "/assets/img/project1/3.png",
+    img: ["/assets/img/project1/1.jpg", "/assets/img/project1/1.png" ,"/assets/img/project1/2.png", "/assets/img/project1/3.png",
     "/assets/img/project1/4.png", "/assets/img/project1/5.png", "/assets/img/project1/6.png"],
-    title: "Oil & Petrolium Plant",
-    description: "Oil & Petrolium Plant .....",
+    title: "Hangers Group",
+    description: `Some of our businesses in Zliten are of different sizes</br>
+    1- It is 36 meters wide, 95 meters long, 9 meters high on the sides, and the center is 11 meters</br>
+    2- 34 meters wide, 50 meters long, 6 meters high from the sides, and the center 7.50 meters</br>
+    3- 24 meters wide, 88 meters long, 7.50 meters high, and the center is 9 meters</br>
+    4- A set of 4 hangers covered with sandwich panels. It stores food items of different sizes</br>
+    Our company is fully prepared to manufacture, equip and install hangars in width from 20 meters to 50 meters and length to infinity without columns in the middle.
+     The company is also ready to construct multi-role steel structures with finishing and complete turnkey concrete works`,
+    year: "2020"
 },
 {
     id: "2",
-    img: "assets/img/beds.jpg" ,
-    title: "Oil & Petrolium Plant",
-    description: "Oil & Petrolium Plant ....."
+    img: ["/assets/img/project2/1.jpg", "/assets/img/project2/1.png", "/assets/img/project2/2.png", "/assets/img/project2/3.png"],
+    title: "Zliten Hangers",
+    description: `One of our businesses in Zliten, with a roof of 3,200 square meters </br>
+    To contact and inquire at the following number: 0912104152`,
+     year: "2020"
 },
 {
     id: "3",
-    img: "assets/img/beds.jpg" ,
-    title: "Oil & Petrolium Plant",
-    description: "Oil & Petrolium Plant ....."
+    img: ["/assets/img/project3/1.jpg", "/assets/img/project3/1.jpg"],
+    title: "STORAGE TANKERS",
+    description: `The technologically advanced range of industrial storage tanks and tanks are designed by relying on manufacturing of stainless steel and malleable steel, and on the basis of cement storage, and these tanks can be obtained according to customer requests`,
+    year: "2020"
 },
 {
     id: "4",
-    img: "assets/img/beds.jpg" ,
-    title: "Oil & Petrolium Plant",
-    description: "Oil & Petrolium Plant ....."
+    img: ["/assets/img/project4/1.jpg","/assets/img/project4/1.png", "/assets/img/project4/2.png", "/assets/img/project4/3.png",
+    "/assets/img/project1/4.png"],
+    title: "Wadi Alrbye",
+    description: null,
+    year: "2020"
 },
 {
     id: "5",
-    img: "assets/img/beds.jpg" ,
-    title: "Oil & Petrolium Plant",
-    description: "Oil & Petrolium Plant ....."
+    img: ["/assets/img/project5/1.jpg","/assets/img/project5/1.png", "/assets/img/project5/2.png", "/assets/img/project5/3.png",
+    "/assets/img/project5/4.png", "/assets/img/project5/5.png"],
+    title: "tariq almushtal",
+    description: null,
+    year: "2021"
 },
 {
     id: "6",
-    img: "assets/img/beds.jpg" ,
-    title: "Oil & Petrolium Plant",
-    description: "Oil & Petrolium Plant ....."
+    img: ["/assets/img/project6/1.jpg","/assets/img/project6/1.png", "/assets/img/project6/2.png", "/assets/img/project6/3.png",
+    "/assets/img/project6/4.png", "/assets/img/project6/5.png"],
+    title: "Hangers Tripoli",
+    description: null,
+    year: "2021"
 }];
 const dataAr = [{
     id: "1",
